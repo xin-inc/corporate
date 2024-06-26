ff8654b179a8176d5ecad570de77c48555174748
xin-yamashita <koichiro.yamashita@xin.inc>
2024-06-27 08:52:55 +0900

    jQuery組み込み/Topアニメーション対応

---
+++ public/js/common.js
@@ -0,0 +1,56 @@
+$(function(){ 
+	// Topページ用アニメーション
+  if($('.md_header--top').length > 0){
+    // headerロゴ　スクロールで小さく
+    $(window).scroll(function(){
+      var obj = $('.header--logo');
+      scroll = $(window).scrollTop();
+      if (scroll > 1) {
+        obj.addClass('isSmall');
+      }else{
+        obj.removeClass('isSmall');
+        if ( $('.bodyWrap').hasClass('body_fixed') ){
+          obj.addClass('isSmall');
+        }
+      }
+    });
+
+    // Topパネルのアニメーション
+    $('.md_contentUnit--panel .bgImgSet .bgImgSection--left .bgImg').each(function(index){
+      $(this).delay(index * 300).queue(function(){
+        $(this).addClass('bgImg--anime');
+      });
+    }); 
+    $('.md_contentUnit--panel .bgImgSet .bgImgSection--right .bgImg').each(function(index){
+      $(this).delay(index * 300).queue(function(){
+        $(this).addClass('bgImg--anime');
+      });
+    }); 
+
+    ScrollReveal().reveal('.js_animeReveal', animeOption );
+    ScrollReveal().reveal('.js_animeReveal01', animeOption );
+    ScrollReveal().reveal('.js_animeReveal02', animeOption );
+    ScrollReveal().reveal('.js_animeReveal03', animeOption );
+    ScrollReveal().reveal('.js_animeReveal04', animeOption );
+    ScrollReveal().reveal('.js_animeReveal05', animeOption );
+    ScrollReveal().reveal('.js_animeReveal06', animeOption );
+    ScrollReveal().reveal('.js_animeReveal07', animeOption );
+    ScrollReveal().reveal('.js_animeReveal08', animeOption );
+    ScrollReveal().reveal('.js_animeReveal09', animeOption );
+    ScrollReveal().reveal('.js_animeReveal10', animeOption );
+    ScrollReveal().reveal('.js_animeReveal11', animeOption );
+    ScrollReveal().reveal('.js_animeReveal12', animeOption );
+
+    //スライドイン
+    var animeOption2 = {origin: 'left',delay:1800,duration: 1600,interval: 400,scale: 1,distance: '80px',reset: false,viewFactor: 0.15,}
+    ScrollReveal().reveal('.js_slideInReveal', animeOption2 );
+
+    //フェードイン
+    var animeOption3 = {origin: 'bottom',delay:1000,duration: 1600,interval: 400,scale: 1,reset: false,viewFactor: 0.15,}
+    ScrollReveal().reveal('.js_fadeInReveal', animeOption3 );
+
+    //オブジェクト用フェードイン
+    var animeOption4 = {origin: 'bottom',delay:1000,duration: 1600,interval: 400,scale: 1,distance: '80px',reset: false,viewFactor: 0.15,}
+    ScrollReveal().reveal('.js_fadeIvReveal_bgImg', animeOption4 );
+  }
+});
